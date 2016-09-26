f5502aced622d38b0ac9889328eb173cf42a5cd9
Jackie Loven <jloven@wpi.edu>
2016-09-25 20:49:38 -0400

    started set method

--- customHashMap/HashMapJLoven.java
+++ customHashMap/HashMapJLoven.java
@@ -8,34 +8,63 @@ public class HashMapJLoven {
 	
 	private KeyValuePair[] hashArray;
 	
-	/** Constructor: an instance for a HashMapJLoven 
-	 * with size hashMapSize. 
-	 * Precondition: n is at least 1. */
+	//  Constructor. hashMapSize must be at least 1.
 	public HashMapJLoven(int hashMapSize) {
 		KeyValuePair[] hash = new KeyValuePair[hashMapSize];
 		hashArray = hash;
 	}
+	
+	//  Places given key value pair in hashmap.
+	//  If value already exists in hashmap, the new value provided will replace it.
+	public boolean set(String aKey, Object aValue) {
+		KeyValuePair keyAndValue = new KeyValuePair();
+		keyAndValue.setKeyAndValue(aKey, aValue);
+		
+		int hashCode = aKey.hashCode();
+		int simpleHashCodeForPlacement = hashCode % this.hashArray.length;
+		if (this.hashArray[simpleHashCodeForPlacement] == null) {
+			this.hashArray[simpleHashCodeForPlacement] = keyAndValue;
+		} else {
+			//  continue here
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		HashMapJLoven hashed = new HashMapJLoven(20);
+		
+		KeyValuePair keyValue = new KeyValuePair();
+		Object rando = new Object();
+		keyValue.setKeyAndValue("hola", rando);
+		hashed.set(keyValue.getKey(), keyValue.getValue());
+	}
+	
 }
 
 class KeyValuePair {
 	
-	private String name;
+	private String key;
 	private Object value;
 	
-	public void setName(String aName) {
-		this.name = aName;
+	public void setKeyAndValue(String aKey, Object aValue) {
+		this.key = aKey;
+		this.value = aValue;
+	}
+	
+	public void setKey(String aKey) {
+		this.key = aKey;
 	}
 	
 	public void setValue(Object aValue) {
 		this.value = aValue;
 	}
 	
-	public String getName() {
-		return this.name;
+	public String getKey() {
+		return this.key;
 	}
 	
 	public Object getValue() {
 		return this.value;
-	}
+}
 	
 }
